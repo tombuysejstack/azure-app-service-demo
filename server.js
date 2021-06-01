c1cf457fa437d891b8a773eee77c5a2feac0984f
tombuyse <tom.buyse5@gmail.com>
2021-06-01 10:52:39 +0200

    updated server.js

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+process.on("SIGTERM", () => {
+  console.info("Interrupted");
+  process.exit(0);
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello environment " + process.env.ENVIRONMENT);
+  res.send("New message 👍 " + process.env.ENVIRONMENT);
 });
 
 app.listen(port, () => {
